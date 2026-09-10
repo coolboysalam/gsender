c6e6a7954cebfc41ac9e4cc9ef9c7da8919f87ed
OmidTaheri <m.aghataher@gmail.com>
2026-09-10 13:06:14 +0330

    Add Persian setting descriptions

---
+++ src/app/src/features/Config/assets/PersianSettingsDescriptions.ts
@@ -0,0 +1,6 @@
+export const PERSIAN_SETTINGS_DESCRIPTIONS: Record<string, string> = {
+    $0: 'مدت زمان پالس STEP بر حسب میکروثانیه.',
+    $1: 'مدت زمان تأخیر قبل از غیرفعال شدن موتورهای محور.',
+    $22: 'تنظیمات و فعال‌سازی Homing دستگاه.',
+    $23: 'جهت حرکت محورها هنگام Homing.',
+};
